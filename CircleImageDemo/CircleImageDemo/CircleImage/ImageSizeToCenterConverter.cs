fe3f5d901218b13763c547854a97702439fbb17b
Sylvain <sylvain.moingeon@outlook.fr>
2020-10-20 17:03:04 +0200

    Add ImageSize to Center (Point) converter

---
+++ CircleImageDemo/CircleImageDemo/CircleImage/ImageSizeToCenterConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CircleImageDemo.CircleImage
+{
+    public class ImageSizeToCenterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double imageSize)
+            {
+                return new Point(imageSize / 2, imageSize / 2);
+            }
+
+            throw new ArgumentException($"{nameof(value)} should be of type double");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
