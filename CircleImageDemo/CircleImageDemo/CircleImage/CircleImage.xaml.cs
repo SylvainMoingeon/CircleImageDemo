608af960c5bafcd89a09bd1c2df264f6af4a8df2
Sylvain <sylvain.moingeon@outlook.fr>
2020-11-09 19:27:54 +0100

    Add alternative code

--- CircleImageDemo/CircleImageDemo/CircleImage/CircleImage.xaml.cs
+++ CircleImageDemo/CircleImageDemo/CircleImage/CircleImage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 using Xamarin.Forms.Xaml;
 
 namespace CircleImageDemo.CircleImage
@@ -29,7 +30,22 @@ namespace CircleImageDemo.CircleImage
              defaultValue: 0.0,
              defaultBindingMode: BindingMode.OneWay,
              propertyChanged: (bindable, oldValue, newValue) =>
-             { });
+             {
+                 //if (bindable is CircleImage circleImage && newValue is double imageSize)
+                 //{
+                 //    double radius = imageSize / 2;
+                 //    EllipseGeometry ellipseGeometry = new EllipseGeometry()
+                 //    {
+                 //        Center = new Point(radius, radius),
+                 //        RadiusX = radius,
+                 //        RadiusY = radius
+                 //    };
+
+                 //    circleImage.HeightRequest = imageSize;
+                 //    circleImage.WidthRequest = imageSize;
+                 //    circleImage.Clip = ellipseGeometry;
+                 //}
+             });
 
         // Gets or sets value of this BindableProperty
         public double ImageSize
